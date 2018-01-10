aea3614a0f791421316d621aa4252ef8974d3880
André Santos <andre@dexdev.com.br>
2018-01-10 01:14:42 -0300

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-require('./config/config'); 
+require('./config/config');
 
-const _ = require('lodash'); 
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { mongoose } = require('./db/mongoose');
@@ -9,7 +9,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 const app = express();
-const port = process.env.PORT; 
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -47,12 +47,12 @@ app.get('/todos/:id', (req, res) => {
 	Todo.findById(id)
 		.then(todo => {
 			if (!todo) {
-                return res.status(404).send(); 
-            }
-            
-			res.send({todo});
+				return res.status(404).send();
+			}
+
+			res.send({ todo });
 		})
-		.catch(e => res.status(400).send(e));	
+		.catch(e => res.status(400).send(e));
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -65,40 +65,53 @@ app.delete('/todos/:id', (req, res) => {
 	Todo.findByIdAndRemove(id)
 		.then(todo => {
 			if (!todo) {
-                return res.status(404).send(); 
-            }
-            
-			res.send({todo});
+				return res.status(404).send();
+			}
+
+			res.send({ todo });
 		})
-		.catch(e => res.status(400).send(e));	
+		.catch(e => res.status(400).send(e));
 });
 
 app.patch('/todos/:id', (req, res) => {
-	let id = req.params.id; 
-	let body = _.pick(req.body, ['text', 'completed']); 
+	let id = req.params.id;
+	let body = _.pick(req.body, ['text', 'completed']);
 
 	if (!ObjectID.isValid(id)) {
 		return res.status(404).send();
 	}
 
-	if(_.isBoolean(body.completed) && body.completed){
+	if (_.isBoolean(body.completed) && body.completed) {
 		body.completedAt = new Date().getTime();
-	}else{
-		body.completed = false; 
+	} else {
+		body.completed = false;
 		body.completedAt = null;
 	}
 
-	Todo.findByIdAndUpdate(id, {$set:body}, {new: true})
-	.then(todo => {
-		if(!todo){
-			return res.status(404).send();
-		}
-		res.send({todo}); 
-	}).catch(e => 
-		res.status(400).send()
-	);
+	Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+		.then(todo => {
+			if (!todo) {
+				return res.status(404).send();
+			}
+			res.send({ todo });
+		})
+		.catch(e => res.status(400).send());
+});
 
-})
+app.post('/users', (req, res) => {
+	const body = _.pick(req.body, ['email', 'password']); 
+	const user = new User(body); 
+
+	user.save()
+	.then(() => user.generateAuthToken())
+	.then(token => {
+		res.header('x-auth', token).send(user)
+	})
+	.catch(e => {
+		console.log(e);
+		res.status(400).send({e})
+	});
+});
 
 app.listen(port, () => {
 	console.log(`Started on port ${port}`);
